11d816e59bd979a5803451dcc61c731e3954e725
Joshua <jcrowe@interpract.com>
2012-06-09 21:38:35 -0700

    add app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrea::Application.config.secret_token = '6a906e5f91aeb443f22749dc997bda5e0438610e1387a99c6b451a27e200ab75dd4dae5aa169721b79c85b50a477352bf888d056b05ebd29f69e13c071e65c1b'
